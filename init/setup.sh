54d8ea333866b819fddb0141c34f330799b217ae
Camilo Cortes H <cortes.camilo@outlook.com>
2023-05-18 20:46:25 -0600

    Adding Load Balancer Controller

--- init/setup.sh
+++ init/setup.sh
@@ -25,6 +25,11 @@ echo 'yq() {
   docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
 }' | tee -a ~/.bashrc && source ~/.bashrc
 
+echo "helm..."
+curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+helm repo add eks https://aws.github.io/eks-charts
+helm repo update
+
 echo "export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)"  >>  ~/.bash_profile
 echo "export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')"  >>  ~/.bash_profile
 .  ~/.bash_profile
@@ -66,6 +71,34 @@ managedNodeGroups:
 EOF
 
 eksctl create cluster -f ekscluster.yaml
+
+echo "AWS Load Balancer Controller..."
+
+eksctl utils associate-iam-oidc-provider \
+    --region ${AWS_REGION} \
+    --cluster eks-copa-hackhaton2023 \
+    --approve
+
+curl -O https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.4.7/docs/install/iam_policy.json
+aws iam create-policy \
+    --policy-name AWSLoadBalancerControllerIAMPolicy \
+    --policy-document file://iam_policy.json
+    
+rm iam_policy.json
+
+eksctl create iamserviceaccount \
+  --cluster eks-copa-hackhaton2023 \
+  --namespace kube-system \
+  --name aws-load-balancer-controller \
+  --attach-policy-arn arn:aws:iam::${ACCOUNT_ID}:policy/AWSLoadBalancerControllerIAMPolicy \
+  --override-existing-serviceaccounts \
+  --approve
+
+helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
+  -n kube-system \
+  --set clusterName=eks-copa-hackhaton2023 \
+  --set serviceAccount.create=false \
+  --set serviceAccount.name=aws-load-balancer-controller 
 
 #aws eks --region $AWS_REGION update-kubeconfig --name eks-copa-hackhaton2023
 
