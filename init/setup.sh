094e2122c865aa28404a38d930b35057edb26d30
Camilo Cortes H <cortes.camilo@outlook.com>
2023-06-02 16:29:53 -0600

    Update setup.sh

--- init/setup.sh
+++ init/setup.sh
@@ -64,7 +64,7 @@ availabilityZones: ["${AZS[0]}", "${AZS[1]}", "${AZS[2]}"]
 managedNodeGroups:
 - name: nodegroup
   desiredCapacity: 3
-  instanceType: m5.large
+  instanceType: m5.xlarge
   ssh:
     enableSsm: true
 
